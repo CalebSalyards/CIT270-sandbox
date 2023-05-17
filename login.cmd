c80e5db610b0d2db1f9c6143600162a9c3b33887
Caleb Salyards <caleb.salyards@outlook.com>
2023-05-17 00:42:51 -0600

    Reorganizing login scripts

--- login.cmd
+++ login.cmd
@@ -1,4 +1,4 @@
 @echo off
 echo Logging in
-curl -v -d "@login.json" POST -H "Content-Type:application/json" https://dev.stedi.me/login
-curl https://dev.stedi.me/validate/4465b669-49b4-4e07-961f-8a4a5211c363
+curl --insecure -d "@login.json" -X POST -H "Content-Type:application/json" http://localhost:3000/login
+@REM curl https://dev.stedi.me/validate/4465b669-49b4-4e07-961f-8a4a5211c363
