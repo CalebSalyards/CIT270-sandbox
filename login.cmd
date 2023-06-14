e656e7353aceeaf8eee5809630854d9b09930196
Caleb Salyards <caleb.salyards@outlook.com>
2023-06-14 15:25:08 -0600

    Update login.cmd

--- login.cmd
+++ login.cmd
@@ -1,4 +1,4 @@
 @echo off
 echo Logging in
-curl --insecure -d "@login.json" -X POST -H "Content-Type:application/json" https://salyards.cit270.com
+curl --insecure -d "@login.json" -X POST -H "Content-Type:application/json" https://salyards.cit270.com/login
 @REM curl https://dev.stedi.me/validate/4465b669-49b4-4e07-961f-8a4a5211c363
